38ab3b272008783371d5b35239aafedcb8dd690e
Jakhongir <jahaiskhakov@gmail.com>
2022-08-21 02:48:26 +0500

    feat: create base-layout

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CustomModule } from 'src/app/modules/custom/custom.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarService } from './services/sidebar.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CustomModule,
   ],
-  providers: [],
+  providers: [
+    SidebarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
