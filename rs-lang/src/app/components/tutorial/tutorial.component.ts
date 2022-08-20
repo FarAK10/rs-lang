c8c1e9d7f1f718e635d4facb6edb9750f3740db7
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-08-21 03:20:43 +0500

    feat: add buttons

--- rs-lang/src/app/components/tutorial/tutorial.component.ts
+++ rs-lang/src/app/components/tutorial/tutorial.component.ts
@@ -5,7 +5,8 @@ export interface Level {
   digit: string,
   title: string
   text: string,
-  words: number
+  words: number,
+  color: string
 }
 
 @Component({
@@ -15,13 +16,15 @@ export interface Level {
 })
 export class TutorialComponent implements OnInit {
 
+  mouseOver = false;
+
   levels: Level[] = [
-    {id: 1, digit: 'A1', title: 'Beginner', text: 'Понимать интонацию и жесты собеседника. Говорить: "London is the capital of Great Britain".', words: 600},
-    {id: 2, digit: 'A2', title: 'Elementary', text: 'Разговаривать с новорожденными детьми. Слушать англоязычную музыку, делая вид что что-то понимаешь.', words: 1200},
-    {id: 3, digit: 'B1', title: 'Pre-Intermediate', text: 'Пройти тест по английскому языку, чтобы устроиться фронтенд-разработчиком.', words: 1800},
-    {id: 4, digit: 'B2', title: 'Intermediate', text: 'Употреблять английские слова в своей речи. Там, где надо и не надо.', words: 2400},
-    {id: 5, digit: 'С1', title: 'Upper-Intermediate', text: 'Мимикрировать под рядового жителя Брайтон-Бич.', words: 3000},
-    {id: 6, digit: 'C2', title: 'Advanced', text: 'Расслабиться. Потому что никто не знает английский на таком уровне, кроме королевы Великобритании.', words: 3600},
+    {id: 1, digit: 'A1', title: 'Beginner', text: 'Понимать интонацию и жесты собеседника. Говорить: "London is the capital of Great Britain".', words: 600, color: "green"},
+    {id: 2, digit: 'A2', title: 'Elementary', text: 'Разговаривать с новорожденными детьми. Слушать англоязычную музыку, делая вид что что-то понимаешь.', words: 1200, color: "orange"},
+    {id: 3, digit: 'B1', title: 'Pre-Intermediate', text: 'Пройти тест по английскому языку, чтобы устроиться фронтенд-разработчиком.', words: 1800, color: "blue"},
+    {id: 4, digit: 'B2', title: 'Intermediate', text: 'Употреблять английские слова в своей речи. Там, где надо и не надо.', words: 2400, color: "yellowgreen"},
+    {id: 5, digit: 'C1', title: 'Upper-Intermediate', text: 'Мимикрировать под рядового жителя Брайтон-Бич.', words: 3000, color: "violet"},
+    {id: 6, digit: 'C2', title: 'Advanced', text: 'Расслабиться. Потому что никто не знает английский на таком уровне, кроме королевы Великобритании.', words: 3600, color: "red"},
   ]
 
   constructor() { }
