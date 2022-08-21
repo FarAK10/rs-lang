c26a2da319f7d9ea9828a40bec9aac0e8beb4ca8
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-08-21 12:12:30 +0500

    fix: change images for styles

--- rs-lang/src/app/components/tutorial/tutorial.component.ts
+++ rs-lang/src/app/components/tutorial/tutorial.component.ts
@@ -27,10 +27,10 @@ export class TutorialComponent implements OnInit {
 
   levels: Level[] = [
     {id: 1, digit: 'A1', title: 'Beginner', text: 'Понимать интонацию и жесты собеседника. Говорить: "London is the capital of Great Britain".', words: 600, color: "red"},
-    {id: 2, digit: 'A2', title: 'Elementary', text: 'Разговаривать с детьми. Слушать англоязычную музыку, делая вид, что что-то понимаешь.', words: 1200, color: "orange"},
+    {id: 2, digit: 'A2', title: 'Elementary', text: 'Разговаривать с детьми. Слушать англоязычную музыку, делая вид, что что-то понимаете.', words: 1200, color: "orange"},
     {id: 3, digit: 'B1', title: 'Pre-Intermediate', text: 'Пройти тест по английскому языку, чтобы устроиться фронтенд-разработчиком.', words: 1800, color: "yellowgreen"},
     {id: 4, digit: 'B2', title: 'Intermediate', text: 'Употреблять английские слова в своей речи. Там, где надо и не надо.', words: 2400, color: "green"},
-    {id: 5, digit: 'C1', title: 'Upper-Intermediate', text: 'Мимикрировать под рядового жителя Брайтон-Бич.', words: 3000, color: "blue"},
+    {id: 5, digit: 'C1', title: 'Upper-Intermediate', text: 'Мимикрировать под рядового жителя окраины Брайтон-Бич.', words: 3000, color: "blue"},
     {id: 6, digit: 'C2', title: 'Advanced', text: 'Расслабиться. Потому что никто не знает английский на таком уровне, кроме королевы Великобритании.', words: 3600, color: "violet"},
   ]
 
