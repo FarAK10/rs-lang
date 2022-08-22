818ef80759b6306e73c4aa09148859993702f93c
Jakhongir <jahaiskhakov@gmail.com>
2022-08-22 23:11:22 +0500

    fix: delete empty constructor and ngOnInit, extract card and team-card to anothers components, extract repeated colors to constants.scss

--- rs-lang/src/app/app.module.ts
+++ rs-lang/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CustomModule } from 'src/app/modules/custom/custom.module'
+import { CustomModule } from 'src/app/modules/custom/materials.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { SidebarService } from './services/sidebar.service';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { TeamCardComponent } from './components/team-card/team-card.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { HomeComponent } from './home/home.component';
     SidebarComponent,
     FooterComponent,
     HomeComponent,
+    CardComponent,
+    TeamCardComponent,
   ],
   imports: [
     BrowserModule,
