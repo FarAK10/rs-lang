b05d23c4a182c1f0a36cbe2ab118334c4c69108d
FarAK10 <botirovfarhod115@gmail.com>
2022-08-26 22:28:15 +0500

    fix: resolve conflicts with sprint-game

--- rs-lang/src/app/app-routing.module.ts
+++ rs-lang/src/app/app-routing.module.ts
@@ -5,15 +5,10 @@ import { HomeComponent } from './components/layout/home/home.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  {path: '', component: HomeComponent},
-  {path: 'tutorial', component: TutorialComponent, canActivate:[AuthGuard]}
-,
   {
     path: 'game',
     loadChildren: () => import('./routings/game/game.module').then((mod) => mod.GameModule),
   },
->>>>>>> sprint-game
 ];
 
 @NgModule({
