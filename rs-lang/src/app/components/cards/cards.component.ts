dc2872bf0867207be71700b6a03899db1a4a81b0
FarAK10 <botirovfarhod115@gmail.com>
2022-08-29 21:42:40 +0500

    refactor:add spaces

--- rs-lang/src/app/components/cards/cards.component.ts
+++ rs-lang/src/app/components/cards/cards.component.ts
@@ -6,16 +6,11 @@ import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
+  constructor(public data: DataService, public apiService: ApiService) {}
 
-  
-  constructor(
-    public data: DataService,
-    public apiService: ApiService,
-    ) { }
-    
   baseUrl = this.apiService.baseUrl + '/';
   voice!: HTMLAudioElement;
 
@@ -34,15 +29,20 @@ export class CardsComponent implements OnInit {
   onCheck(e: Event) {
     const target = e.target as HTMLElement;
     this.data.currentLevel = Object.assign({}, this.data.levels[Number(target.id)]);
-    this.apiService.getWords(String(this.data.currentLevel.id), '0').subscribe(value => this.data.words = JSON.parse(JSON.stringify(value)));
+    this.apiService
+      .getWords(String(this.data.currentLevel.id), '0')
+      .subscribe((value) => (this.data.words = JSON.parse(JSON.stringify(value))));
     this.data.page = 0;
   }
 
   play(e: Event, Meaning: string, Example: string): void {
     e.preventDefault();
-    let audio = [Meaning, Example];
+    let audio = [
+      Meaning,
+      Example,
+    ];
     let count = 0;
-    
+
     const playAudio = () => {
       if (this.voice) this.voice.pause();
       const currentAudio = new Audio();
@@ -52,33 +52,34 @@ export class CardsComponent implements OnInit {
       count += 1;
       if (count >= audio.length) currentAudio.remove;
       currentAudio.onended = () => playAudio();
-    }
+    };
 
     playAudio();
   }
 
   changePage(page: string) {
-    switch(page) {
-      case ('0') :
+    switch (page) {
+      case '0':
         this.data.page = 0;
         break;
-      case ('-1') :
+      case '-1':
         this.data.page = this.data.page -= 1;
         break;
-      case ('+1') :
+      case '+1':
         this.data.page = this.data.page += 1;
         break;
-      case ('29') :
+      case '29':
         this.data.page = 29;
         break;
-      default :
+      default:
         this.data.page = this.data.page;
     }
-    this.apiService.getWords(String(this.data.currentLevel.id), String(this.data.page)).subscribe(value => this.data.words = JSON.parse(JSON.stringify(value)));
+    this.apiService
+      .getWords(String(this.data.currentLevel.id), String(this.data.page))
+      .subscribe((value) => (this.data.words = JSON.parse(JSON.stringify(value))));
   }
 
   ngOnInit(): void {
     window.addEventListener('scroll', this.onScroll);
   }
-
 }
