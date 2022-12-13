3d9a666c5ada79d37f1ea361814eb76227379d45
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-12-13 18:49:34 +0500

    fix: correct link heroku

--- rs-lang/src/app/services/api.service.ts
+++ rs-lang/src/app/services/api.service.ts
@@ -10,7 +10,7 @@ import { DataService } from './data.service';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  baseUrl = 'https://app-rs-lang.herokuapp.com';
+  baseUrl = 'https://app-lang.herokuapp.com/';
   // baseUrl = 'http://localhost:8088';
 
   post<T>(url: string, body: T): Observable<T> {
